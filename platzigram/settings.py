aa5f87c046af07e70b855131c4b08d41857a1728
rlence <lencericardo@gmail.com>
2020-04-27 19:47:52 +0200

    agregando nueva app para crear usarios admin

--- platzigram/settings.py
+++ platzigram/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     # My local APPS
     'posts',
+    'users'
 ]   
 
 MIDDLEWARE = [
---
+++ users/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django.contrib.auth.models import User
+# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    website = models.URLField(blank=True, max_length=200)
+    biograypy = models.TextField(blank=True)
+    phone_number = models.CharField(blank=True, max_length=20)
+    picture = models.ImageField( upload_to='users/pictures', blank=True, null=True)
+    created = models.DateTimeField(auto_now=True)
+    modified = models.DateTimeField( auto_now=True)
+
+    def __str__(self):
+        """return username"""
+        return self.user.username
