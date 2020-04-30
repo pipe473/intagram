56de21d886cb0fa0f6a755dc5f8a6e360500c909
rlence <lencericardo@gmail.com>
2020-04-30 14:45:23 +0200

    agregando middleware para que el usuario complete su registro

--- users/views.py
+++ users/views.py
@@ -50,6 +50,8 @@ def signup(request):
 
     return render(request, 'users/signup.html')
 
+def update_profile(request):
+    return render(request, 'users/update_profile.html')
 
 @login_required
 def logout_view(request):
